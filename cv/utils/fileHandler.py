9bcefca66502806fe0e5cdeec60bd50bdc870310
Niklas Schumann <68606032+Flamtky@users.noreply.github.com>
2022-10-27 23:17:04 +0200

    Update fileHandler.py

--- cv/utils/fileHandler.py
+++ cv/utils/fileHandler.py
@@ -19,7 +19,7 @@ def loadFolder(directory) -> list:
     return ret
 
 
-def saveImage(image, *, name=None, path='out/', extension='.png'):
+def saveImage(image, name=None, *, path='out/', extension='.png'):
     import cv2 as cv
     import os
     import time

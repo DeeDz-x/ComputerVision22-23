de3ed1c36d0cae03977aa904dd6f2f178b8cbcc7
Deedz <dennis.edler@fh-bielefeld.de>
2022-11-16 20:17:34 +0100

    MOG2 looking fine

--- cv/processing/bgsubtraction.py
+++ cv/processing/bgsubtraction.py
@@ -1,9 +1,43 @@
 import cv2 as cv
+import numpy as np
+
+
 
 def openCVSubMOG2(file):
 
     backsub = cv.createBackgroundSubtractorMOG2()
     video = cv.VideoCapture(file)
+
+    backsub.setDetectShadows(False)
+    backsub.setVarThreshold(200)
+
+    while True:
+        ret, frame = video.read()
+        if frame is None:
+            break
+
+        channel = cv.GaussianBlur(frame, (5,5), 1.2)
+
+        fgMask = backsub.apply(channel, learningRate = 0)
+
+        # cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
+        # cv.putText(frame, str(video.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+        #            cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+        cv.imshow("Frame", frame)
+        cv.imshow("FG Mask", fgMask)
+
+        keyboard = cv.waitKey(30)
+        if keyboard == 27:
+            break
+
+
+def openCVSubKNN(file):
+
+    video = cv.VideoCapture(file)
+    backsub = cv.createBackgroundSubtractorKNN()
+
+    backsub.setDetectShadows(False)
 
     while True:
         ret, frame = video.read()
@@ -20,10 +54,14 @@ def openCVSubMOG2(file):
             break
 
 
-def openCVSubGMG():
-    pass
 
 def ownSub():
     pass
 
-openCVSubMOG2()
+if __name__ == "__main__":
+    openCVSubMOG2(r"C:\Users\DeeDz\ComputerVision22-23\images\data_ms2\1\input\1_2.avi")
+    openCVSubMOG2(r"C:\Users\DeeDz\ComputerVision22-23\images\data_ms2\2\input\2_2.avi")
+    openCVSubMOG2(r"C:\Users\DeeDz\ComputerVision22-23\images\data_ms2\3\input\3_2.avi")
+    openCVSubMOG2(r"C:\Users\DeeDz\ComputerVision22-23\images\data_ms2\4\input\4_2.avi")
+
+

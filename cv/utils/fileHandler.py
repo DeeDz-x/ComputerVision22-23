8e9ad2a6d4d49ed210d10e0b52e64e1b5763d109
Flamtky <n.schumann@rz-online.de>
2022-11-16 18:10:45 +0100

    added type hinting

--- cv/utils/fileHandler.py
+++ cv/utils/fileHandler.py
@@ -2,9 +2,10 @@ import os
 import time
 
 import cv2 as cv
+from numpy import ndarray
 
 
-def loadFrames(directory) -> list:
+def loadFrames(directory: str) -> list[list[ndarray]]:
     ret = [[], []]
     for i, folder in enumerate(os.listdir(directory)):
         for file in os.listdir(directory + folder):
@@ -12,20 +13,17 @@ def loadFrames(directory) -> list:
     return ret
 
 
-def loadFolder(directory) -> list:
-    ret = []
-    for _ in os.listdir(directory):
-        ret.append([])
+def loadFolder(directory: str) -> list[list[list[ndarray]]]:
+    ret = [[] for _ in range(len(os.listdir(directory)))]
 
     for i, folder in enumerate(os.listdir(directory)):
         ret[i] = loadFrames(directory + folder + '\\')
     return ret
 
 
-def saveImage(image, name=None, *, path='out/', extension='.png'):
+def saveImage(image: ndarray, name: str = None, *, path: str = 'out/', extension: str = '.png') -> None:
     if not os.path.exists('out'):
         os.makedirs('out')
 
     name = name if name else str(time.time())
-
     cv.imwrite(path + name + extension, image)

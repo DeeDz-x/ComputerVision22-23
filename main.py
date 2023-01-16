f089a91955abc281e34ae3d9bd6c61c648035ace
Flamtky <n.schumann@rz-online.de>
2023-01-16 22:49:15 +0100

    added histo history

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import cv2 as cv
 import numpy as np
 
 from cv.features.detections import confidenceFilter
+from cv.features.tracking import getHisto
 from cv.processing.evaluation import evalMOTA
 from cv.utils.fileHandler import loadFolderMileStone4
 from cv.utils.video import playImageAsVideo
-from cv.features.tracking import getHisto
 
 IMAGES_PATH = os.path.dirname(os.path.abspath(__file__)) + '\\images\\'
 DISPLAY = False  # displays the image and waits for a key press
@@ -45,7 +45,7 @@ def detect():
         frame_counter = 0
         highestBoxId = 0
         history_size = 10
-        history = {} # key == id; value == list of last 10 histograms
+        history = {}  # key == id; value == list of last 'history_size' histograms
         while True:
             ret, frame = video.read()
             frame_counter += 1
@@ -54,15 +54,21 @@ def detect():
 
             gt_boxes_in_frame = gt_dict[frame_counter]
             det_boxes_in_frame = det_dict[frame_counter]
+            # calc histo for each box
+            histos_in_frame = []
+            for box in det_boxes_in_frame:
+                hist = getHisto(frame[round(box.top):round(box.bottom), round(box.left):round(box.right)])
+                histos_in_frame.append(hist)
+
             if frame_counter == 1:
                 for det_i, box in enumerate(det_boxes_in_frame):
                     box.box_id = det_i + 1
                     highestBoxId = det_i + 1
+                    history[box.box_id] = [histos_in_frame[det_i]]
             else:
-                # finds the best match for each detection
                 prev_det_boxes_in_frame = det_dict[frame_counter - 1]
-                # shortest distance
-                for det_box in det_boxes_in_frame:
+                for det_i, det_box in enumerate(det_boxes_in_frame):
+                    # get nearest neighbour
                     min_dist = sys.maxsize
                     min_box = None
                     for prev_det_box in prev_det_boxes_in_frame:
@@ -72,23 +78,36 @@ def detect():
                             min_box = prev_det_box
                     if min_box is None:
                         raise Exception('No box found')
+
                     # checks if id is already taken, by checking if id is in the current frame or distance is too high
-                    if min_box.box_id in [box.box_id for box in det_boxes_in_frame] or min_dist > 100:
-
-                        histo = getHisto(
-                            frame[min_box.y:min_box.y + min_box.h, min_box.x:min_box.x + min_box.w]
-                        )
-
-                        # check if id is in history
-                        if min_box.box_id in history:
-                            pass
-
-
+                    if min_box.box_id in [box.box_id for box in det_boxes_in_frame]:
                         # if taken, create new id
                         highestBoxId += 1
                         det_box.box_id = highestBoxId
+                    elif min_dist > 75:
+                        cur_hist = histos_in_frame[det_i]
+                        # if distance is too high, check history, if histogram similar enough, use that id, else create new id
+                        for id_in_history, hists in history.items():
+                            avg_sim = 0
+                            for hist in hists:
+                                avg_sim += cv.compareHist(hist, cur_hist, cv.HISTCMP_CORREL)
+                            avg_sim /= len(hists)
+                            if avg_sim > 0.5:
+                                det_box.box_id = id_in_history
+                                break
+                        else:
+                            highestBoxId += 1
+                            det_box.box_id = highestBoxId
                     else:
                         det_box.box_id = min_box.box_id
+
+                    # add to history
+                    if det_box.box_id in history:
+                        history[det_box.box_id].append(histos_in_frame[det_boxes_in_frame.index(det_box)])
+                        if len(history[det_box.box_id]) > history_size:
+                            history[det_box.box_id].pop(0)
+                    else:
+                        history[det_box.box_id] = [histos_in_frame[det_boxes_in_frame.index(det_box)]]
 
             if DISPLAY:
                 overlay = None
